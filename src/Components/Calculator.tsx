c585e0e0d06851a8cdd2381479236e62e7630667
Senyo-SA <sadzivor@gmail.com>
2025-05-27 19:31:23 +0100

    Buttons and Display

--- src/Components/Calculator.tsx
+++ src/Components/Calculator.tsx
@@ -1,9 +1,20 @@
+
+
 function Calculate(){
+
+    const numbers: any = [1, 2, 3, 'C', 4, 5, 6, '+', 7, 8, 9, '-', '.', 0, '=', 'X']
+    const display = []
+
+    for(let num:number = 0; num < 16; num++){
+        display.push(<button className="Input">{numbers[num]}</button>)
+    }
 
 return(
     <div className="Calculator">
 
+       <form action="get" className="Display_area">  </form>
        
+       <div className="Input_area"> {display} </div>
 
 
     </div>

14bf356b3f218f4103ea9b48f92d6c3554ea10b1
Senyo-SA <sadzivor@gmail.com>
2025-06-06 19:52:55 +0100

    Displaying calculations

--- Frontend/src/Components/Calculator.tsx
+++ Frontend/src/Components/Calculator.tsx
@@ -5,36 +5,48 @@ function Calculate() {
 
   const [num_display, setNum_display] = useState('')
 
-  const [digits , setDigits] : any = useState('0')
-
-  function show_digits(value: any){
-    console.log(value)
-    setDigits(digits + value)
-
-    if (value == '='){
-      setDigits(num_display )
-    }
-
-    if (value == 'DEL'){
-      setDigits('0')
-    }
-    
-  }
+  const [digits , setDigits] : any = useState('')
 
   async function show_num(){
     try{
-      const response = await Api.get('/display');
-      setNum_display((response.data.expression.toString()));
+      const response = await Api.get('/calculate');
+      console.log(response)
+      setNum_display((response.data.expression));
     }catch(error){
-      console.error('error fetching fruits', error)
+      console.error('error processing numbers', error)
     }
   }
+  
+
+  async function show_digits(value: any){
+
+    if (value == '='){
+
+      setNum_display(digits)
+      setDigits('')
+
+      try{
+        await Api.post('/calculate', {expression: num_display})
+        show_num()
+      }catch(error){
+        console.error('calculation error', error)
+      }
+    }
+    else {
+      setDigits(digits + value)
+    }
+  }
+
+
 
   useEffect(() => {
     show_num();
   }, []);
 
+  // Initialise array of numbers and signs
   const numbers: any = [1, 2, 3, "+", 4, 5, 6, "-", 7, 8, 9, "*", ".", 0, "C", "÷", "DEL", "√", "%", "="];
+
+  // Initialise empty array for button components
   const display = [];
 
   for (let num: number = 0; num < 20; num++) {
@@ -45,11 +57,14 @@ function Calculate() {
     );
   }
 
+  console.log(digits)
+  console.log(num_display)
+
 
   return (
     <div className="Calculator">
       <form action="get" className="Display_area">
-        <input className='Input_Area' placeholder={digits} type='text'/>
+        <input className='Input_Area'  placeholder={num_display} type='text' value={digits}/>
       </form>
 
       <div className="Input_area"> {display} </div>
